19a6d12c89c43c386909dd4798f4a63956287331
cuisongliu <cuisongliu@qq.com>
2017-05-20 16:13:55 +0800

    相关的工具类

---
+++ src/main/java/com/cuisongliu/springboot/core/util/encrypt/BASE64URL.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cuisongliu.springboot.core.util.encrypt;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ *  对Url进行BASE64加密
+ */
+public class BASE64URL {
+    public static String base64UrlEncode(byte[] simple) {
+
+        String s = new String(Base64.encodeBase64(simple)); // Regular base64 encoder
+        s = s.split("=")[0]; // Remove any trailing '='s
+        s = s.replace('+', '-'); // 62nd char of encoding
+        s = s.replace('/', '_'); // 63rd char of encoding
+        return s;
+    }
+
+    public static byte[] base64UrlDecode(String cipher) {
+        String s = cipher;
+        s = s.replace('-', '+'); // 62nd char of encoding
+        s = s.replace('_', '/'); // 63rd char of encoding
+        switch (s.length() % 4) { // Pad with trailing '='s
+            case 0:
+                break; // No pad chars in this case
+            case 2:
+                s += "==";
+                break; // Two pad chars
+            case 3:
+                s += "=";
+                break; // One pad char
+            default:
+                System.err.println("Illegal base64url String!");
+        }
+        return Base64.decodeBase64(s); // Standard base64 decoder
+    }
+
+    public static void main(String[] args) {
+        System.out.println(base64UrlEncode("cj201611272256".getBytes()));
+        System.out.println(new String(base64UrlDecode("Y2oyMDE2MTEyNzIyNTY")));
+    }
+}
